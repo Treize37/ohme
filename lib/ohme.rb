08ee05a52c42df210efa2a9fad3d802cf5763a94
Fabrice Carrega <fabrice.carrega@treize37.com>
2024-12-12 10:59:21 +0100

    Initial commit

---
+++ lib/ohme.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "ohme/version"
+
+module Ohme
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/ohme_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Ohme do
+  it "has a version number" do
+    expect(Ohme::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
