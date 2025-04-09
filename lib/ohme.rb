4bc31be3db54ecc92f0c5268d8ff5845867cf7a2
Fabrice Carrega <fabrice.carrega@treize37.com>
2025-04-09 12:59:23 +0200

    Updated code & version

--- lib/ohme.rb
+++ lib/ohme.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 require_relative 'ohme/version'
+require_relative 'ohme/configuration'
+require_relative 'ohme/client'
+require_relative 'ohme/api/contact'
 
 # Ohme API client gem main module.
 module Ohme
