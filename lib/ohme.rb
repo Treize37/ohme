90e90e0dc5f209a9b52226e1bc2a25ecb65aaf2a
Fabrice Carrega <fabrice.carrega@treize37.com>
2025-05-19 11:56:33 +0200

    Corrected lib requirements

--- lib/ohme.rb
+++ lib/ohme.rb
@@ -4,6 +4,7 @@ require_relative 'ohme/version'
 require_relative 'ohme/configuration'
 require_relative 'ohme/client'
 require_relative 'ohme/api/contact'
+require_relative 'ohme/api/payment'
 
 # Ohme API client gem main module.
 module Ohme
